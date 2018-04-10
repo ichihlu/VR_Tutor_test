08b52382b87802e8ce4e78fa15ecef5e30bad921
ichihlu <38261507+ichihlu@users.noreply.github.com>
2018-04-10 20:51:42 +0200

    initital commit

---
+++ VRSTUFFTHING/Assets/AnimTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTrigger : MonoBehaviour {
+
+    Animator anim;
+
+    //This variable has the keyword "public" in front of it, whcich in this context means that has become a property which all other classes can get access to.
+    //This also means that it is accessable in the inspector in Unity.
+    public float speed = 0;
+
+
+
+
+    //Event that happens before start, one time during the lifetime of the Gameobject
+    private void Awake() {
+        
+    }
+
+    //one time
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+
+    //Every frame (Framerate varies between different computers).
+	void Update () {
+
+        //The Input class handles all inputs from the user. Check the documentation pages online to understand how expansive it is ;)
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            anim.Play("Bounce", 0);
+        }
+
+        //Here we utilize Time.deltatime to compansate for the different framerates by multiplying the speed with how long the last frame took to compute.
+        //This is a very typical technique when working with values that should change at a constant pase, or when doing locomotive motion (As in this example).
+        transform.position += Vector3.forward * Time.deltaTime * speed;
+	}
+
+    //After ever update
+    //Very typically used when some calculation depends on the rest of the game having reached a "conclusion" the frame in question.
+    private void LateUpdate() {
+        
+    }
+
+    //This is a self made Function (or Method) which can be called from other classes (notice the keyword "public" again)
+    public void PlayAnimation() {
+        anim.Play("Bounce", 0);
+    }
+
+    //Like PlayAnimation(), this is a function I made to show parameters (in this case a and b).
+    //Unlike PlayAnimation() this Function has a return type of "int", this means that the function needs to conclude and return some int value.
+    int Add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ VRSTUFFTHING/Assets/Raycaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour {
+
+    private void Update() {
+        Debug.DrawRay(transform.position, transform.forward * 15, Color.red);
+
+        //This is a declaration of an empty variable of Type RaycastHit.
+        //This means that we have declared some use for this later, but have yet to utilize it in any way.
+        RaycastHit hit;
+
+        //Here I'm doing lots of stuff simultaniously... sorry
+        //An if statement is a way of checking wheither or not something is true or not. If true, the code inside the Curly Brackets{} will run.
+        //Otherwise the code will skip past and continue whichever codeblock it is in.
+
+        //Physics.Raycast() is a function you can google to learn more about (or select it and press F12 to look into the class it belongs to)
+        //print() is a fucntion which writes to the console window in Unity, use this very often!
+        //GetComponent is a way of getting the reference to some other component in the GameObject that we are currently in.
+        if(Physics.Raycast(transform.position, transform.forward, out hit)) {
+            print("Yey, we hit " + hit.collider.name);
+            if (hit.collider.GetComponent<AnimTrigger>()) {
+                print("And the thing has a Trigger");
+                hit.collider.GetComponent<AnimTrigger>().PlayAnimation();
+            }
+        }
+    }
+}
